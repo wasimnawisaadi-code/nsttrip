a78ed5249b9319e415ac2ccfc8ce645e03f105b3
Nawi Saadi CRM <wasimnawisaadi@gmail.com>
2026-05-30 17:27:40 +0400

    fix: force production build with absolute synchronization

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
+// Production Build Sync: 2026-05-30 17:27
+// Enforcing 'One Rule' - 100k Limit - All Templates Sync
+import React from 'react';
 import App from "./App.tsx";
 import "./index.css";
 
