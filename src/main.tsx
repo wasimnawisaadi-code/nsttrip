ba5eba075e3f73a2047417d1941ccbcd7dc9e558
Nawi Saadi CRM <wasimnawisaadi@gmail.com>
2026-05-30 15:22:02 +0400

    build: trigger production deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Production Build Trigger: 2026-05-30 15:20
 createRoot(document.getElementById("root")!).render(<App />);
